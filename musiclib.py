84ae30e24ab9c35a8da6c744d41f63eddd7276e0
simha-codes <vishwasimha8@gmail.com>
2025-11-24 18:02:15 +0530

    Update musiclib.py

--- musiclib.py
+++ musiclib.py
@@ -1,6 +1,9 @@
 music= {
     'sanam':"https://www.youtube.com/watch?v=kKljXVVkgS4&list=RDkKljXVVkgS4&start_radio=1",
     'ragile':"https://www.youtube.com/watch?v=dRBowc83jVs&list=RDdRBowc83jVs&start_radio=1",
-    
+    'believer':"https://www.youtube.com/watch?v=R4AbzwYOmNE&list=RDR4AbzwYOmNE&start_radio=1",
+    'Ayudha Pooja':"https://www.youtube.com/watch?v=HZ_Q20ir-gg&list=RDHZ_Q20ir-gg&start_radio=1",
+    'Chikri Chikri':"https://www.youtube.com/watch?v=vVDp1ulBKIk&list=RDvVDp1ulBKIk&start_radio=1",
+    'Desi Kalakaar':"https://www.youtube.com/watch?v=KhnVcAC5bIM&list=RDKhnVcAC5bIM&start_radio=1",
     'funk':"https://open.spotify.com/track/55YYhNqfMTIpPulNc0lLme?si=ff564b2c05b548db"
 }
