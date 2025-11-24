64ecd1edeb0dfd4cd61fdb0da0726c734a3408d0
simha-codes <vishwasimha8@gmail.com>
2025-11-24 17:37:16 +0530

    Create musiclib.py

---
+++ musiclib.py
@@ -0,0 +1,6 @@
+music= {
+    'sanam':"https://www.youtube.com/watch?v=kKljXVVkgS4&list=RDkKljXVVkgS4&start_radio=1",
+    'ragile':"https://www.youtube.com/watch?v=dRBowc83jVs&list=RDdRBowc83jVs&start_radio=1",
+    
+    'funk':"https://open.spotify.com/track/55YYhNqfMTIpPulNc0lLme?si=ff564b2c05b548db"
+}
